6697975cba4b69dae16f7dd931ded422cc898904
aissat <mr.aissat@gmail.com>
2024-12-23 01:03:30 +0100

    add` CounterBloc` implementation with `increment` and `decrement` functionality

---
+++ lib/counter/state/counter_bloc.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:unistate_adapter/unistate_adapter.dart';
+
+import 'abstract.dart';
+
+class CounterBloc extends Bloc<CounterEvent, int>
+    with UnistateBlocMixin<CounterEvent, int>
+    implements Counter {
+  CounterBloc() : super(0) {
+    on<Increment>((event, emit) => emit(state + 1));
+    on<Decrement>((event, emit) => emit(state - 1));
+  }
+
+  @override
+  void decrement() {
+    add(Decrement());
+  }
+
+  @override
+  void increment() {
+    add(Increment());
+  }
+}
+
+sealed class CounterEvent {}
+
+class Decrement extends CounterEvent {}
+
+class Increment extends CounterEvent {}
