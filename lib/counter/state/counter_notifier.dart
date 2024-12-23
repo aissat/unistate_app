16e44ce72e0c34ec610405ed734f00cd74a4a9af
aissat <mr.aissat@gmail.com>
2024-12-23 01:04:29 +0100

    add `CounterProvider` implementation with `increment` and `decrement` methods

---
+++ lib/counter/state/counter_notifier.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/foundation.dart';
+import 'package:unistate_adapter/unistate_adapter.dart';
+
+import 'abstract.dart';
+
+class CounterProvider extends ChangeNotifier
+    with UnistateNotifierMixin<int>
+    implements Counter {
+  CounterProvider() : super();
+
+  @override
+  int get initialValue => 0;
+
+  @override
+  void decrement() {
+    value--;
+    // notifyListeners();
+  }
+
+  @override
+  void increment() {
+    value++;
+    // notifyListeners();
+  }
+}
